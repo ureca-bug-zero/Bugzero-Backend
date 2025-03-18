294ddb25715dee9bf169d0acd91b37a87f159471
이서현 <iseohyeon@iseohyeon-ui-MacBookAir.local>
2025-03-18 15:18:48 +0900

    feat: #8 FriendListDto 설정

---
+++ src/main/java/com/uplus/bugzerobackend/dto/FriendListDto.java
@@ -0,0 +1,11 @@
+package com.uplus.bugzerobackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class FriendListDto {
+    private Integer friendId;
+    private String friendName;
+}
