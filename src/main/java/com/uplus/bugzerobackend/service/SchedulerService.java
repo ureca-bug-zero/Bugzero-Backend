f521111162224a02f50e236720cdbd614a379266
SujinRo <shtnwls1111@ewhain.net>
2025-03-18 17:08:01 +0900

    feat: #1 SchedulerService에 시간이 되면 mission todo가 올라가는 코드 추가

--- src/main/java/com/uplus/bugzerobackend/service/SchedulerService.java
+++ src/main/java/com/uplus/bugzerobackend/service/SchedulerService.java
@@ -1,9 +1,12 @@
 package com.uplus.bugzerobackend.service;
 
 import lombok.RequiredArgsConstructor; //final 필드에서 자동으로 생성자 만듦
+import lombok.extern.slf4j.Slf4j; //log
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.Trigger;
 import org.springframework.scheduling.TriggerContext;
@@ -13,19 +16,25 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import org.springframework.stereotype.Service;
 
+import com.uplus.bugzerobackend.controller.TodoListController;
+import com.uplus.bugzerobackend.domain.TodoList;
+import com.uplus.bugzerobackend.dto.TodoListDto;
+import com.uplus.bugzerobackend.dto.UserDto;
+import com.uplus.bugzerobackend.mapper.TodoListMapper;
+import com.uplus.bugzerobackend.mapper.UserMapper;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 // SchedulingConfigurer: 스케줄링 설정을 프로그래밍 방식으로 제어할 수 있는 인터페이스
-public class SchedulerConfiguration implements SchedulingConfigurer{
+public class SchedulerService implements SchedulingConfigurer{
 	@Value("${schedule.cron}") // application.properties에 값 지정
 	private String cronExpression;
 	
 	@Value("${schedule.use}")
 	private boolean isSchedulerEnabled;
 	
-	private final ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler(); // 실제 작업 실행할 스레드 풀 (기본 1개의 스레드)
-	
-	private static final Logger log = LoggerFactory.getLogger(SchedulerConfiguration.class);
+	private final ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler(); // 실제 작업 실행할 스레드 풀 (기본 1개의 스레드) 
 	
 	@Override
 	public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
@@ -39,7 +48,26 @@ public class SchedulerConfiguration implements SchedulingConfigurer{
 		}
 	}
 	
+	private final UserMapper userMapper;
+	private final TodoListMapper todoListMapper;
+	
 	private void runScheduledTask() {
+		List<UserDto> userList = userMapper.findAll();
+		Random random = new Random();
+		int randomNum = random.nextInt(45000 - 1000) + 1000 ; // 1000 - 44999 사이 랜덤 숫자
+		
+		for (UserDto user : userList) {
+			TodoListDto todo = new TodoListDto();
+			todo.setUserId(user.getId());
+			todo.setContent("오늘의 미션 문제");
+			todo.setLink("https://www.acmicpc.net/problem/" + randomNum);
+			todo.setChecked(false);
+			todo.setMission(true);
+			todo.setDate(LocalDate.now());
+			
+			todoListMapper.insert(todo);
+		}
+
 		log.info("현재 시간: {}", System.currentTimeMillis());
 	}
 	
