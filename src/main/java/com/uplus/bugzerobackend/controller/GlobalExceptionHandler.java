4536dd60dd9268c0c1c313a39d4e6c41cbe7f177
Jetty-Lee <jetty0188@gmail.com>
2025-03-18 14:47:01 +0900

    refactor: 에러 메시지 logging 추가

--- src/main/java/com/uplus/bugzerobackend/controller/GlobalExceptionHandler.java
+++ src/main/java/com/uplus/bugzerobackend/controller/GlobalExceptionHandler.java
@@ -19,6 +19,7 @@ public class GlobalExceptionHandler {
     // 400 Bad Request   - 필수 매개변수 누락.
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ApiResponseDto<Object>> handleIllegalArgumentException(IllegalArgumentException ex) {
+    	ex.printStackTrace();
         return ResponseEntity.badRequest()
                 .body(ApiResponseDto.failure("E400", ex.getMessage()));
     }
@@ -26,6 +27,7 @@ public class GlobalExceptionHandler {
     // 401 - Unauthorized  - 토큰 오류
     @ExceptionHandler(SecurityException.class)
     public ResponseEntity<ApiResponseDto<Object>> handleUnauthorizedException(SecurityException ex) {
+    	ex.printStackTrace();
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                 .body(ApiResponseDto.failure("E401", ex.getMessage()));
     }
@@ -33,6 +35,7 @@ public class GlobalExceptionHandler {
     // 404 - Not Found  - 요청한 데이터에 해당하는 리소스 없음
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ApiResponseDto<Object>> handleEntityNotFoundException(EntityNotFoundException ex) {
+    	ex.printStackTrace();
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(ApiResponseDto.failure("E404", ex.getMessage()));
     }
@@ -40,13 +43,15 @@ public class GlobalExceptionHandler {
     // 500 Internal Server Error - 논리적 오류 처리(데이터 불일치)
     @ExceptionHandler(IllegalStateException.class)
     public ResponseEntity<ApiResponseDto<Object>> handleIllegalStateException(IllegalStateException ex) {
+    	ex.printStackTrace();
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ApiResponseDto.failure("E500", ex.getMessage()));
     }
 
     // 500 - 기타 서버 내부 오류
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiResponseDto<Object>> handleGeneralException() {
+    public ResponseEntity<ApiResponseDto<Object>> handleGeneralException(Exception ex) {
+    	ex.printStackTrace();
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ApiResponseDto.failure("E500", "서버 내부 오류가 발생했습니다. 잠시 후 다시 시도해주세요."));
     }
