08f75cde93b4418aef45626c50bf76e1b7fa2eba
ahn_minji <ahn_minji@naver.com>
2025-03-18 12:59:48 +0900

    docs: TodoListService.java 주석 변경

--- src/main/java/com/uplus/bugzerobackend/service/TodoListService.java
+++ src/main/java/com/uplus/bugzerobackend/service/TodoListService.java
@@ -6,7 +6,7 @@ import java.util.List;
 public interface TodoListService {
     void insert(TodoList todoList);  // TodoList 추가
     void update(TodoList todoList);  // TodoList 수정
-    TodoList search(Integer id);        // TodoList 조회
+    TodoList search(Integer id);     // TodoList 조회
     List<TodoList> searchAll();      // 모든 TodoList 조회
-    void remove(Integer id);            // TodoList 삭제
+    void remove(Integer id);         // TodoList 삭제
 }
