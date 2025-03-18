149499954e4c2a32e8843723cd95d9ed93442124
이서현 <iseohyeon@iseohyeon-ui-MacBookAir.local>
2025-03-18 15:14:38 +0900

    feat: schema.sql 추가

---
+++ src/main/resources/db/schema.sql
@@ -0,0 +1,33 @@
+CREATE TABLE IF NOT EXISTS `user` (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    weekScore INT NOT NULL,
+    `rank` INT NOT NULL
+    );
+
+
+CREATE TABLE IF NOT EXISTS friend (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    friendList JSON NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
+    );
+
+CREATE TABLE IF NOT EXISTS todo_list (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    date DATE NOT NULL,
+    content VARCHAR(255) NOT NULL,
+    isMission BOOLEAN NOT NULL,
+    isChecked BOOLEAN NOT NULL,
+    link VARCHAR(512),
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
+    );
+
+CREATE TABLE IF NOT EXISTS calendar (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    date DATE NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
+    );
