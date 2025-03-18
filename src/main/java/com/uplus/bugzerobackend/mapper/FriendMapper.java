b4fd1958c3e89697a463fa7381b14fde4e4b0c56
이서현 <iseohyeon@iseohyeon-ui-MacBookAir.local>
2025-03-18 15:16:04 +0900

    feat: #8 FriendMapper 구현

--- src/main/java/com/uplus/bugzerobackend/mapper/FriendMapper.java
+++ src/main/java/com/uplus/bugzerobackend/mapper/FriendMapper.java
@@ -3,9 +3,7 @@ package com.uplus.bugzerobackend.mapper;
 import com.uplus.bugzerobackend.domain.Friend;
 import org.apache.ibatis.annotations.Mapper;
 
-import java.sql.SQLException;
-
 @Mapper
 public interface FriendMapper {
-    Friend searchFriendList(int id) throws SQLException;
+    Friend getFriendById(int userId);
 }
