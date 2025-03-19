7bff858d431f69d599c3fa12f612d3bb6f6a0e42
ahn_minji <ahn_minji@naver.com>
2025-03-19 11:35:55 +0900

    fix: #5 Collections import

--- src/main/java/com/uplus/bugzerobackend/service/TodoListServiceImpl.java
+++ src/main/java/com/uplus/bugzerobackend/service/TodoListServiceImpl.java
@@ -7,6 +7,7 @@ import com.uplus.bugzerobackend.TodoListException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service 
