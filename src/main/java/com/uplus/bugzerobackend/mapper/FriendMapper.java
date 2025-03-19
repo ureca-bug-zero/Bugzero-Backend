081bbd0831192f501953fa7ccae1abf0f2ec6e83
SujinRo <shtnwls1111@ewhain.net>
2025-03-19 22:36:56 +0900

    feat: #26 FriendMapper에 deleteFriend 코드 추가

--- src/main/java/com/uplus/bugzerobackend/mapper/FriendMapper.java
+++ src/main/java/com/uplus/bugzerobackend/mapper/FriendMapper.java
@@ -1,9 +1,13 @@
 package com.uplus.bugzerobackend.mapper;
 
 import com.uplus.bugzerobackend.domain.Friend;
+
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface FriendMapper {
     Friend getFriendById(int userId);
+    void deleteFriend(Map<String, Object> params);
 }
