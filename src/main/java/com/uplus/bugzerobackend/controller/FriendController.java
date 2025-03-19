483b5ead90daaaaccba118afbdc3a3dfcc24b8cd
SujinRo <shtnwls1111@ewhain.net>
2025-03-19 22:38:50 +0900

    feat: #26 FriendController에 deleteFriend 코드 추가

--- src/main/java/com/uplus/bugzerobackend/controller/FriendController.java
+++ src/main/java/com/uplus/bugzerobackend/controller/FriendController.java
@@ -8,17 +8,24 @@ import com.uplus.bugzerobackend.mapper.UserMapper;
 import com.uplus.bugzerobackend.service.FriendService;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/friend")
 @RequiredArgsConstructor
+
 public class FriendController {
     private final UserMapper userMapper;
     private final FriendService friendService;
@@ -37,4 +44,17 @@ public class FriendController {
 
         return ResponseEntity.ok(ApiResponseDto.success("친구 리스트 조회 성공", friendList.isEmpty() ? null : friendList));
     }
+    
+    @PutMapping("/{userId}/{friendId}")
+    public ResponseEntity<ApiResponseDto<Object>> delete(@PathVariable("userId") int userId, @PathVariable("friendId") int friendId){
+    	try {
+    	friendService.deleteFriend(userId, friendId);
+    	return ResponseEntity.ok(ApiResponseDto.success("친구 삭제 성공", null));
+    	}catch (Exception e) {
+            log.error("친구 삭제 중 오류 발생", e);  // 예외
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                                 .body(ApiResponseDto.failure("친구 삭제 실패", e.getMessage()));
+        }
+    }
 }
+
