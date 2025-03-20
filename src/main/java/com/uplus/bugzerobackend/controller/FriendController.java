cc12ab683b7a6519709caa60bc4454e44ae70628
SujinRo <shtnwls1111@ewhain.net>
2025-03-20 18:37:04 +0900

    fix: #26 friend_list에서 선택한 친구 없을 시, 예외 처리하는 코드 오류 수정

--- src/main/java/com/uplus/bugzerobackend/controller/FriendController.java
+++ src/main/java/com/uplus/bugzerobackend/controller/FriendController.java
@@ -18,19 +18,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @Slf4j
 @RestController
 @RequestMapping("/friend")
 @RequiredArgsConstructor
-
 public class FriendController {
-    private final UserMapper userMapper;
+	private final UserMapper userMapper;
     private final FriendService friendService;
-
-
+    
     @GetMapping("/{userId}")
     public ResponseEntity<ApiResponseDto<List<FriendListDto>>> getFriends(@PathVariable Integer userId) {
         // 유저 존재 여부 확인
@@ -38,10 +35,8 @@ public class FriendController {
         if(user == null) {
             throw new EntityNotFoundException("존재하지 않는 유저입니다.");
         }
-
         // 친구 리스트 가져오기
         List<FriendListDto> friendList = friendService.getFriend(userId);
-
         return ResponseEntity.ok(ApiResponseDto.success("친구 리스트 조회 성공", friendList.isEmpty() ? null : friendList));
     }
     
@@ -49,12 +44,11 @@ public class FriendController {
     public ResponseEntity<ApiResponseDto<Object>> delete(@PathVariable("userId") int userId, @PathVariable("friendId") int friendId){
     	try {
     	friendService.deleteFriend(userId, friendId);
-    	return ResponseEntity.ok(ApiResponseDto.success("친구 삭제 성공", null));
+    	return ResponseEntity.ok(ApiResponseDto.success("친구 삭제를 성공했습니다.", null));
     	}catch (Exception e) {
             log.error("친구 삭제 중 오류 발생", e);  // 예외
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                                 .body(ApiResponseDto.failure("친구 삭제 실패", e.getMessage()));
+                                 .body(ApiResponseDto.failure("친구 삭제를 실패했습니다.", e.getMessage()));
         }
     }
 }
-
