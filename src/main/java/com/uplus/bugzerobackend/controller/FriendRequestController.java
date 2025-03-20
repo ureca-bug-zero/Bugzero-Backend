41337d13e99ceb4d326debbc84c04fe2e7e8ed41
이서현 <iseohyeon@iseohyeon-ui-MacBookAir.local>
2025-03-20 20:55:38 +0900

    FriendRequestController 인증 추가

--- src/main/java/com/uplus/bugzerobackend/controller/FriendRequestController.java
+++ src/main/java/com/uplus/bugzerobackend/controller/FriendRequestController.java
@@ -3,6 +3,8 @@ package com.uplus.bugzerobackend.controller;
 import com.uplus.bugzerobackend.dto.ApiResponseDto;
 import com.uplus.bugzerobackend.dto.FriendRequestDto;
 import com.uplus.bugzerobackend.service.FriendRequestService;
+import com.uplus.bugzerobackend.service.JwtTokenService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,11 +16,14 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class FriendRequestController {
     private final FriendRequestService friendRequestService;
+    private final JwtTokenService jwtTokenService;
 
 
     @PostMapping("/request")
     public ResponseEntity<ApiResponseDto<FriendRequestDto>> insertFriendRequest(
-            @RequestParam String email, @RequestParam Integer senderId) {
+            HttpServletRequest request, @RequestParam String email) {
+        Integer senderId = jwtTokenService.getUserId(request);
+
         friendRequestService.insertFriendRequest(email, senderId);
 
         return ResponseEntity.ok(ApiResponseDto.success("친구 요청을 성공하였습니다.", null));
@@ -26,15 +31,20 @@ public class FriendRequestController {
 
     @PostMapping("/response/refuse")
     public ResponseEntity<ApiResponseDto<Void>> refuseFriendRequest(
-            @RequestParam Integer receiverId, @RequestParam Integer senderId) {
-        friendRequestService.deleteFriendRequest(receiverId, senderId);
-
+            HttpServletRequest request, @RequestParam Integer senderId) {
+        Integer receiverId = jwtTokenService.getUserId(request);
+        try {
+            friendRequestService.deleteFriendRequest(receiverId, senderId);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalStateException("친구 거절 후 friendRequest DB 업데이트에 실패하였습니다.");
+        }
         return ResponseEntity.ok(ApiResponseDto.success("친구 요청 거절에 성공하였습니다.", null));
     }
 
     @PostMapping("/response/accept")
     public ResponseEntity<ApiResponseDto<Void>> acceptFriendRequests(
-            @RequestParam Integer receiverId, @RequestParam Integer senderId) {
+            HttpServletRequest request, @RequestParam Integer senderId) {
+        Integer receiverId = jwtTokenService.getUserId(request);
         friendRequestService.updateFriendRequest(receiverId, senderId);
         try {
             friendRequestService.deleteFriendRequest(receiverId, senderId);
