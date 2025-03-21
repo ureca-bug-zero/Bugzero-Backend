7ac6a10647efda18bd64782cfbe2f8892594cd9f
이서현 <iseohyeon@iseohyeon-ui-MacBookAir.local>
2025-03-21 09:43:12 +0900

    fix: #54 Request 수정

--- src/main/java/com/uplus/bugzerobackend/controller/CalendarController.java
+++ src/main/java/com/uplus/bugzerobackend/controller/CalendarController.java
@@ -32,22 +32,11 @@ public class   CalendarController {
     // 월별 투두 개수 조회
     @PostMapping("/monthly")
     public ResponseEntity<ApiResponseDto<Map<String, Object>>> getMonthlyProgress(
-            HttpServletRequest request,  @RequestBody CalendarRequestDto calendarRequestDto) {
+            HttpServletRequest request,  @RequestBody String yearMonth) {
         Integer userId = jwtTokenService.getUserId(request);
-        List<TodoListDto> todoLists = todoListService.searchByUserIdAndYearMonth(userId, calendarRequestDto.getYearMonth());
+        List<TodoListDto> todoLists = todoListService.searchByUserIdAndYearMonth(userId, yearMonth);
 
-        Map<String, Object> response = calendarService.processMonthly(todoLists, calendarRequestDto);
+        Map<String, Object> response = calendarService.processMonthly(todoLists, yearMonth);
         return ResponseEntity.ok(ApiResponseDto.success("캘린더 조회를 성공하였습니다.", response));
     }
-
-
-    // 특정 날짜 투두 달성 개수 조회
-//    @PostMapping("/daily")
-//    public ResponseEntity<Map<String, Object>> getDailyTodoStatus(HttpServletRequest request, @RequestBody CalendarRequestDto calendarRequestDto) {
-//        Integer userId = jwtTokenService.getUserId(request);
-//        List<TodoListDto> todoLists = todoListService.searchAll(userId, calendarRequestDto.getDate());
-//        Map<String, Object> response = calendarService.processDaily(todoLists, calendarRequestDto);
-//        return ResponseEntity.ok(response);
-//    }
-
 }
